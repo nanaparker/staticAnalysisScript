8a5e86a71ed42f53d82b2f91a34954b4a7bbff00
Nana Parker <44725238+nanaparker@users.noreply.github.com>
2024-11-20 11:25:48 +0000

    Add files via upload

---
+++ staticAnalysis.ps1
@@ -0,0 +1,22 @@
+# The goal of this is to run through the first few Static Analysis steps to have time to do other things
+
+mkdir "Output"
+Write-Host "Getting Hash"
+md5sum $args[0] > Output/FileHash.txt
+sha1sum $args[0] > Output/FileHash.txt
+sha256sum $args[0] > Output/FileHash.txt
+
+Write-Host "Running CAPA"
+capa -v -q $args[0] > Output/capaOutput.txt
+Write-Host "---------------"
+
+Write-Host "Running EXIFTOOL"
+exiftool -q $args[0] >> Output/exifOutput.txt
+Write-Host "---------------"
+
+Write-Host "Running FLOSS"
+floss -n 6 -q $args[0] >> Output/flossOutput.txt
+Write-Host "---------------"
+
+Write-Host "Opening Detect It Easy"
+die $args[0]
