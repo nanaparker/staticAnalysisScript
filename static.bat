c0e4248f95d3515932927d788fe130fd06b2449b
Nana Parker <44725238+nanaparker@users.noreply.github.com>
2024-11-20 11:28:39 +0000

    Update static.bat

--- static.bat
+++ static.bat
@@ -1,11 +1,22 @@
 @echo off
 set arg1=%1
+
 mkdir "Output"
+
+echo "Getting Hash"
+md5sum %arg1% > Output/FileHash.txt
+sha1sum %arg1% >> Output/FileHash.txt
+sha256sum %arg1% >> Output/FileHash.txt
+echo ---------------
+
 echo Running CAPA
 capa -v -q %arg1% > Output/capaOutput.txt
 echo ---------------
+
 echo Running EXIFTOOL
-exiftool -q %arg1% >> Output/exifOutput.txt
+exiftool -q %arg1% > Output/exifOutput.txt
+echo ---------------
+
 echo Running FLOSS
-floss -n 6 -q %arg1% >> Output/flossOutput.txt
+floss -n 6 -q %arg1% > Output/flossOutput.txt
 echo ---------------
