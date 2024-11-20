0eb4a898d75d566d9d316411ec9ba1b8bc31143d
Nana Parker <44725238+nanaparker@users.noreply.github.com>
2024-11-20 13:39:43 +0000

    Update staticAnalysis.ps1

--- staticAnalysis.ps1
+++ staticAnalysis.ps1
@@ -3,8 +3,8 @@
 mkdir "Output"
 Write-Host "Getting Hash"
 md5sum $args[0] > Output/FileHash.txt
-sha1sum $args[0] > Output/FileHash.txt
-sha256sum $args[0] > Output/FileHash.txt
+sha1sum $args[0] >> Output/FileHash.txt
+sha256sum $args[0] >> Output/FileHash.txt
 Write-Host "---------------"
 
 Write-Host "Running CAPA"
@@ -12,11 +12,11 @@ capa -v -q $args[0] > Output/capaOutput.txt
 Write-Host "---------------"
 
 Write-Host "Running EXIFTOOL"
-exiftool -q $args[0] >> Output/exifOutput.txt
+exiftool -q $args[0] > Output/exifOutput.txt
 Write-Host "---------------"
 
 Write-Host "Running FLOSS"
-floss -n 6 -q $args[0] >> Output/flossOutput.txt
+floss -n 6 -q $args[0] > Output/flossOutput.txt
 Write-Host "---------------"
 
 Write-Host "Opening Detect It Easy"
